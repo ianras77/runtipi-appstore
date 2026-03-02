da3c66026bf787186f337872f2c491bc16d28da4
ianras77 <ianras@yahoo.com>
2026-03-02 02:01:03 +0000

    Add wordpress_elderseth

--- apps/jogmania/api/app/redis_client.py
+++
@@ -1,7 +0,0 @@
-import redis
-
-from .settings import settings
-
-
-def get_redis():
-  return redis.Redis.from_url(settings.redis_url, decode_responses=True)
--- apps/jogmania/repo/apps/web/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@jogmania/shared", "@jogmania/api-client"]
-};
-
-module.exports = nextConfig;
